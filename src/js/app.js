7ae35915ce4aa0aec0c5eefa1631407834458cef
Natan_VilasBoas <86916855+Natan200-2@users.noreply.github.com>
2023-12-26 17:29:33 -0300

    project's begin

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const newsContainer = document.getElementById('newsContainer');
+    
+    function getAllNews(){
+        let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=59e016d648a44ed4ac0e452c04cf2730'
+    
+        let req = new Request(url)
+    
+        fetch(req)
+        .then(function(response) {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.articles)
+
+            let count = 0;
+
+            let row = document.createElement('div')
+            row.className = "row";
+
+            data.articles.forEach(news => {
+                const newsElement = document.createElement('div');
+
+                newsElement.className = 'col-md-4';
+
+                newsElement.innerHTML= `
+                <div class="card mb-5" style="width: 18rem;">
+                    <img src="${news.urlToImage}" class="card-img-top" alt="${news.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.description}</p>
+                    </div>
+                </div>`
+                row.appendChild(newsElement);
+
+                count++
+
+                if(count == 3){
+                    newsContainer.appendChild(row);
+
+                    count = 0;
+                    row = document.createElement('div')
+                    row.className = "row"
+                }
+            })
+            if (count > 0 ){
+                newsContainer.appendChild(row)
+            }
+        })
+        .catch((error) =>{
+            console.error('Erro na solicitação: ', error);
+        })
+    }
+    
+    getAllNews();
+})
