2d9df3921d8520fc5f34ef3f1e4e6640d85b4ef8
Natan_VilasBoas <86916855+Natan200-2@users.noreply.github.com>
2024-01-02 13:10:09 -0300

    Dark-mode applied

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
 console.log(Date.now)
 
 document.addEventListener('DOMContentLoaded', function(){
+
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+    toggleButton.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+        
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.removeItem('dark-mode');
+        }
+    });
+
     const newsContainer = document.getElementById('newsContainer');
     getAllNews();
 })
